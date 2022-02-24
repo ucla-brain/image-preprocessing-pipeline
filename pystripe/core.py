502888687bab72af45784c0bcfd2645a755a2a64
Keivan Moradi <k.moradi@gmail.com>
2022-02-23 18:35:21 -0800

    * better find the optimum chunk value

--- pystripe/core.py
+++ pystripe/core.py
@@ -993,7 +993,7 @@ def batch_filter(
     manager = Manager()
     args_list = manager.list(args_list)
     num_images_need_processing = len(args_list)
-    while chunks > 1 and num_images_need_processing // chunks < workers:
+    while chunks > 1 and num_images_need_processing % (chunks * workers) > 10:
         chunks //= 2
     print(f'{datetime.now()}: {num_images_need_processing} images need processing.\n\t'
           f'Setting up {workers} workers. Each worker processes {chunks} images at a time.\n'
