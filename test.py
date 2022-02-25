73d0882d9bfe808c93b10612819052c4e02bc20e
Keivan Moradi <k.moradi@gmail.com>
2022-02-24 17:47:40 -0800

    * testing handling ctrl + c

--- test.py
+++ test.py
@@ -1,20 +1,63 @@
-from pathlib import Path
-import os
-import re
-def glob_re(pattern: str, path: Path):
-    """Recursively find all files having a specific name
-        path: Path
-            Search path
-        pattern: str
-            regular expression to search the file name.
-    """
-    regexp = re.compile(pattern, re.IGNORECASE)
-    for p in os.scandir(path):
-        # p = Path(p)
-        if p.is_file() and regexp.search(p.name):
-            yield Path(p.path)
-        elif p.is_dir(follow_symlinks=False):  # 
-            yield from glob_re(pattern, p.path)
+from multiprocessing import Pool, freeze_support
+from time import sleep
+import tqdm
+import signal
+from functools import wraps
 
-for p in glob_re(r"\.(?:tiff?|raw)$", Path(r"C:\test-25G")):
-    print(p)
+
+def handle_ctrl_c(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        global ctrl_c_entered
+        if not ctrl_c_entered:
+            signal.signal(signal.SIGINT, default_sigint_handler) # the default
+            try:
+                return func(*args, **kwargs)
+            except KeyboardInterrupt:
+                ctrl_c_entered = True
+                return KeyboardInterrupt()
+            finally:
+                signal.signal(signal.SIGINT, pool_ctrl_c_handler)
+        else:
+            return KeyboardInterrupt()
+    return wrapper
+
+
+@handle_ctrl_c
+def slowly_square(i):
+    sleep(1)
+    return i*i
+
+
+def pool_ctrl_c_handler(*args, **kwargs):
+    global ctrl_c_entered
+    ctrl_c_entered = True
+
+
+def init_pool():
+    # set global variable for each process in the pool:
+    global ctrl_c_entered
+    global default_sigint_handler
+    ctrl_c_entered = False
+    default_sigint_handler = signal.signal(signal.SIGINT, pool_ctrl_c_handler)
+
+
+def main():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    with Pool(processes=61, initializer=init_pool) as pool:
+        results = tqdm.tqdm(
+            pool.map(slowly_square, range(100)),
+            total=100,
+            ascii=True
+        )
+        list(results)
+        # if any(list(map(lambda x: isinstance(x, KeyboardInterrupt), results))):
+        #     print('Ctrl-C was entered.')
+        # else:
+        #     print(results)
+
+
+if __name__ == '__main__':
+    freeze_support()
+    ctrl_c_entered, default_sigint_handler = None, None
+    main()
