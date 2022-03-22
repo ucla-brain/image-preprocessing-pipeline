9178bd39c388663588c21194253c5cce499bd6ff
Keivan Moradi <k.moradi@gmail.com>
2022-03-22 10:31:46 -0700

    New: convert ims to tif using h5py

--- convert_to_terafly.py
+++ convert_to_terafly.py
@@ -2,23 +2,28 @@ import os
 import sys
 import psutil
 import subprocess
-from pystripe.core import batch_filter
+from pystripe.core import batch_filter, convert_to_8bit_fun
 from multiprocessing import freeze_support
 from pathlib import Path
 from time import time
 from platform import uname
 import re
+import h5py
+from tifffile import imsave
+from tqdm import tqdm
+from numpy import where
 
 
 if __name__ == '__main__':
-    source_folder = Path(sys.argv[1])
-    final_tiff_folder = Path(sys.argv[2])
+    source = Path(sys.argv[1])
+    tif_2d_folder = Path(sys.argv[2])
     dir_tera_fly = Path(sys.argv[3])
-    print(f"source folder is {final_tiff_folder}")
-    print(f"TeraFly folder is {dir_tera_fly}")
+
+    tif_2d_folder.mkdir(exist_ok=True)
+    dir_tera_fly.mkdir(exist_ok=True)
 
     freeze_support()
-    dir_tera_fly.mkdir(exist_ok=True, parents=True)
+    # dir_tera_fly.mkdir(exist_ok=True, parents=True)
     PyScriptsPath = Path(r"./TeraStitcher/pyscripts")
     if sys.platform == "win32":
         # print("Windows is detected.")
@@ -66,38 +71,55 @@ if __name__ == '__main__':
         new_path = new_path.replace(" ", r"\ ").replace("(", r"\(").replace(")", r"\)").replace("/", "\\\\")
         return new_path
 
-    batch_filter(
-        source_folder,
-        final_tiff_folder,
-        workers=92,
-        # sigma=[foreground, background] Default is [0, 0], indicating no de-striping.
-        sigma=(0, 0),
-        # level=0,
-        wavelet="db10",
-        crossover=10,
-        # threshold=-1,
-        compression=('ZLIB', 1),  # ('ZSTD', 1) conda install imagecodecs
-        flat=None,
-        dark=100,
-        # z_step=voxel_size_z,  # z-step in micron. Only used for DCIMG files.
-        # rotate=False,
-        lightsheet=True,
-        artifact_length=150,
-        # percentile=0.25,
-        # convert_to_16bit=False,  # defaults to False
-        convert_to_8bit=True,
-        bit_shift_to_right=1,
-        continue_process=True,
-        dtype='uint16',
-        tile_size=(37088, 22014),
-        down_sample=(2, 2),
-        new_size=(15651, 9290),
-        timeout=None
-    )
+
+    if source.is_file() and source.suffix.lower() == ".ims":
+        with h5py.File(source, "r") as file:
+            dark = 120
+            bit_shift = 0
+            images = file[f"DataSet/ResolutionLevel 0/TimePoint 0/Channel {1}/Data"]
+            list(tqdm(
+                map(lambda args:
+                    imsave(
+                        tif_2d_folder / f"img_{args[0]:04}.tif",
+                        convert_to_8bit_fun(
+                            where(args[1] > dark, args[1] - dark, 0),
+                            bit_shift_to_right=bit_shift)
+                    ),
+                    enumerate(images)),
+                total=len(images), ascii=True, smoothing=0.05, mininterval=1.0, unit="img", desc="ims2tif"
+            ))
+    else:
+        batch_filter(
+            source,
+            tif_2d_folder,
+            workers=96,
+            # sigma=[foreground, background] Default is [0, 0], indicating no de-striping.
+            sigma=(0, 0),
+            # level=0,
+            wavelet="db10",
+            crossover=10,
+            # threshold=-1,
+            compression=('ZLIB', 0),  # ('ZSTD', 1) conda install imagecodecs
+            flat=None,
+            dark=100,
+            # z_step=voxel_size_z,  # z-step in micron. Only used for DCIMG files.
+            # rotate=False,
+            lightsheet=False,
+            artifact_length=150,
+            # percentile=0.25,
+            # convert_to_16bit=False,  # defaults to False
+            convert_to_8bit=True,
+            bit_shift_to_right=1,
+            continue_process=True,
+            dtype='uint16',
+            tile_size=None,
+            down_sample=None,
+            new_size=None,
+            timeout=None
+        )
 
     command = [
-        f"mpiexec -np 48 python -m mpi4py {paraconverter}",
-        # f"{teraconverter}",
+        f"mpiexec -np 4 python -m mpi4py {paraconverter}",
         "--sfmt=\"TIFF (series, 2D)\"",
         "--dfmt=\"TIFF (tiled, 3D)\"",
         "--resolutions=\"012345\"",
@@ -109,7 +131,7 @@ if __name__ == '__main__':
         # "--height=256",
         # "--width=256",
         # "--depth=256",
-        f"-s={final_tiff_folder}",  # destination_folder
+        f"-s={tif_2d_folder}",  # destination_folder
         f"-d={dir_tera_fly}",
     ]
     start_time = time()
