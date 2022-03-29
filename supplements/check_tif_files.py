b71b383afb6685dd705a80b2dbc6b1aa18e13787
Keivan Moradi <k.moradi@gmail.com>
2022-03-28 23:33:26 -0700

    * support for timeout

--- supplements/check_tif_files.py
+++ supplements/check_tif_files.py
@@ -4,27 +4,27 @@ Read tif files and if the file was damaged print its name.
 
 from pathlib import Path
 from multiprocessing import freeze_support
-from concurrent.futures import ProcessPoolExecutor
-from pystripe.core import imread_tif_raw
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from pystripe.core import imread_tif_raw, calculate_cores_and_chunk_size, glob_re
 
-
-source = Path(r"Y:\SmartSPIM_Data\2022_03_15\20220315_14_15_20_SW220203_03_LS_15x_1000z_lightsheet_cleaned_tif_bitshift.b3.r0_downsampled\Ex_642_Em_680")
+source = Path(
+    r"Y:\SmartSPIM_Data\2022_03_22\20220322_11_15_40_SA210705_01_L_Hem_LS_10x_500z_stitched\Ex_561_Em_600_tif")
 
 
 def test_image(file: Path):
     try:
-        return imread_tif_raw(file) if file.is_file() and file.suffix == ".tif" else None
+        if file.is_file() and file.suffix == ".tif":
+            with ThreadPoolExecutor(1) as tp:
+                future = tp.submit(imread_tif_raw, (file, ))
+                future.result(timeout=200)
     except Exception:
         print(file)
 
 
 if __name__ == "__main__":
     freeze_support()
-    with ProcessPoolExecutor(max_workers=61) as pool:
-        for x_folder in source.iterdir():
-            if x_folder.is_dir():
-                for y_folder in x_folder.iterdir():
-                    if y_folder.is_dir():
-                        # y_folder_source2 = source2.joinpath(y_folder.relative_to(source))
-                        # list(pool.map(test_image, map(lambda f: y_folder_source2/f.name, y_folder.iterdir())))
-                        list(pool.map(test_image, y_folder.iterdir(), chunksize=16))
+    with ProcessPoolExecutor(61) as pool:
+        files = list(glob_re(r"\.(?:tiff?|raw)$", source))
+        cores, chunks = calculate_cores_and_chunk_size(
+            len(files), cores=61, pool_can_handle_more_than_61_cores=False)
+        list(pool.map(test_image, files, chunksize=chunks))
