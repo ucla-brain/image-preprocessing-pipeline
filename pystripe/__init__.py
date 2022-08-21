fb3342edff04b0661eaa27d888d4d181e69441cc
Keivan Moradi <k.moradi@gmail.com>
2022-08-20 22:52:26 -0700

    * code cleanup * better documentation * new: destriping horizontally and vertically * performance improvement

--- pystripe/__init__.py
+++ pystripe/__init__.py
@@ -1,6 +1,5 @@
 from pystripe.core import filter_streaks, batch_filter, \
                           gaussian_filter, hist_match, max_level, foreground_fraction, \
                           imread_tif_raw, imread_dcimg, imsave_tif, \
-                          waverec, wavedec, \
-                          fft, ifft, fft2, ifft2, magnitude, normalize_flat
+                          fft, normalize_flat
 
