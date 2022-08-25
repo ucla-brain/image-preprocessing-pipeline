7f37a906fc0f669be630138de7981fecb8a487a9
Keivan Moradi <k.moradi@gmail.com>
2022-08-24 19:39:20 -0700

    * minor updated to notifications.

--- pystripe/core.py
+++ pystripe/core.py
@@ -828,7 +828,7 @@ class MultiProcess(Process):
 
 def progress_manager(process_list: List[Process], progress_queue: Queue, workers: int, total: int, desc="PyStripe"):
     return_code = 0
-    print(f'{datetime.now()}: using {workers} workers. {total} images need preprocessing.')
+    print(f'{datetime.now()}: using {workers} workers. {total} images need be processed.')
     progress_bar = tqdm(total=total, ascii=True, smoothing=0.05, mininterval=1.0, unit="images", desc=desc)
 
     def kill_processes(list_of_processes):
