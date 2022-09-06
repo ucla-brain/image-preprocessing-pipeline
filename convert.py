5d93a2091d301e35e529605ed9510c8e25bb4334
Keivan Moradi <k.moradi@gmail.com>
2022-09-06 12:27:50 -0700

    * make the convert function smarter for handling arguments * detect cpu instructions * convert tif to teraFly * teraFly downsampling method is max

--- convert.py
+++ convert.py
@@ -5,13 +5,15 @@ import subprocess
 from pystripe.core import batch_filter
 from time import time
 from platform import uname
-from multiprocessing import freeze_support, cpu_count
-from numpy import zeros, ndarray, where
+from multiprocessing import freeze_support
+from numpy import ndarray, where
 from pathlib import Path
 from pystripe.core import convert_to_8bit_fun
 from process_images import correct_path_for_cmd, correct_path_for_wsl
-from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace
+from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace, BooleanOptionalAction
 from parallel_image_processor import parallel_image_processor
+from supplements.cli_interface import PrintColors
+from cpufeature.extension import CPUFeature
 
 os.environ['MKL_NUM_THREADS'] = '1'
 os.environ['NUMEXPR_NUM_THREADS'] = '1'
@@ -27,31 +29,41 @@ def process_img(image: ndarray, dark: int = 120, bit_shift: int = 0):
 
 
 def main(args: Namespace):
-    source = Path(args.input)
+    input_path = Path(args.input)
+    if not args.input or not input_path.exists():
+        print(f"{PrintColors.FAIL}Input path is not valid.{PrintColors.ENDC}")
+        raise RuntimeError
+
     tif_2d_folder = Path(args.tif)
-    tif_2d_folder.mkdir(exist_ok=True, parents=True)
-    dir_tera_fly = None
+    if not args.tif:
+        tif_2d_folder.mkdir(exist_ok=True, parents=True)
+
+    dir_tera_fly = Path(args.teraFly)
     if args.teraFly:
-        dir_tera_fly = Path(args.teraFly)
         dir_tera_fly.mkdir(exist_ok=True, parents=True)
-    ims_file = None
-    if args.imaris:
-        ims_file = Path(args.imaris)
+
+    ims_file = Path(args.imaris)
 
     return_code = 0
-    if source.is_file() and source.suffix.lower() == ".ims":
+    if input_path.is_file() and input_path.suffix.lower() == ".ims":
+        if not args.tif:
+            print(f"{PrintColors.FAIL} tif path is needed for ims to any format{PrintColors.ENDC}")
+            raise RuntimeError
         return_code = parallel_image_processor(
             process_img,
-            str(source),
-            str(tif_2d_folder),
+            input_path,
+            tif_2d_folder,
             (),
             {"dark": args.dark, "bit_shift": args.bit_shift},
             max_processors=args.nthreads,
             channel=args.channel
         )
-    elif source.is_dir():
+    elif input_path.is_dir() and (args.dark > 0 or args.convert_to_8bit):
+        if not args.tif:
+            print(f"{PrintColors.FAIL} tif path is needed for processing 2D tif series{PrintColors.ENDC}")
+            raise RuntimeError
         return_code = batch_filter(
-            source,
+            input_path,
             tif_2d_folder,
             workers=args.nthreads,
             # sigma=[foreground, background] Default is [0, 0], indicating no de-striping.
@@ -69,7 +81,7 @@ def main(args: Namespace):
             artifact_length=150,
             # percentile=0.25,
             # convert_to_16bit=False,  # defaults to False
-            convert_to_8bit=True,
+            convert_to_8bit=args.convert_to_8bit,
             bit_shift_to_right=args.bit_shift,
             continue_process=True,
             dtype='uint16',
@@ -78,16 +90,19 @@ def main(args: Namespace):
             new_size=None,
             timeout=None
         )
+    elif input_path.is_dir():
+        tif_2d_folder = input_path
+
     assert return_code == 0
 
-    if dir_tera_fly is not None:
+    if args.teraFly:
         command = [
             f"mpiexec -np {args.nthreads} python -m mpi4py {paraconverter}",
             "--sfmt=\"TIFF (series, 2D)\"",
             "--dfmt=\"TIFF (tiled, 3D)\"",
             "--resolutions=\"012345\"",
             "--clist=0",
-            "--halve=mean",
+            "--halve=max",
             # "--noprogressbar",
             # "--sparse_data",
             # "--fixed_tiling",
@@ -101,7 +116,7 @@ def main(args: Namespace):
         subprocess.call(" ".join(command), shell=True)
         print(f"elapsed time = {round((time() - start_time) / 60, 1)}")
 
-    if ims_file is not None:
+    if args.imaris:
         file = tif_2d_folder / sorted(tif_2d_folder.glob("*.tif"))[0]
         command = [
             f"" if sys.platform == "win32" else "wine",
@@ -126,11 +141,14 @@ def main(args: Namespace):
 
 if __name__ == '__main__':
     freeze_support()
+    cpu_instruction = "SSE2"
+    for item in ["SSE2", "AVX", "AVX2", "AVX512f"]:
+        cpu_instruction = item if CPUFeature[item] else cpu_instruction
     PyScriptsPath = Path(r"./TeraStitcher/pyscripts")
     if sys.platform == "win32":
         # print("Windows is detected.")
         psutil.Process().nice(psutil.IDLE_PRIORITY_CLASS)
-        TeraStitcherPath = Path(r"TeraStitcher/Windows/avx512")
+        TeraStitcherPath = Path(r"TeraStitcher/Windows")/cpu_instruction
         os.environ["PATH"] = f"{os.environ['PATH']};{TeraStitcherPath.as_posix()}"
         os.environ["PATH"] = f"{os.environ['PATH']};{PyScriptsPath.as_posix()}"
         terastitcher = "terastitcher.exe"
@@ -138,7 +156,7 @@ if __name__ == '__main__':
     elif sys.platform == 'linux' and 'microsoft' not in uname().release.lower():
         print("Linux is detected.")
         psutil.Process().nice(value=19)
-        TeraStitcherPath = Path(r"./TeraStitcher/Linux/AVX512")
+        TeraStitcherPath = Path(r"./TeraStitcher/Linux")/cpu_instruction
         os.environ["PATH"] = f"{os.environ['PATH']}:{TeraStitcherPath.as_posix()}"
         os.environ["PATH"] = f"{os.environ['PATH']}:{PyScriptsPath.as_posix()}"
         terastitcher = "terastitcher"
@@ -166,18 +184,20 @@ if __name__ == '__main__':
     )
     parser.add_argument("--input", "-i", type=str, required=True,
                         help="Path to input image or path")
-    parser.add_argument("--tif", "-t", type=str, required=True,
+    parser.add_argument("--tif", "-t", type=str, required=False, default='',
                         help="Path to tif output path")
-    parser.add_argument("--teraFly", "-f", type=str, default='',
+    parser.add_argument("--teraFly", "-f", type=str, required=False, default='',
                         help="Path to output teraFly path")
-    parser.add_argument("--imaris", "-o", type=str, default='',
+    parser.add_argument("--imaris", "-o", type=str, required=False, default='',
                         help="Path to 8-bit imaris output file")
-    parser.add_argument("--nthreads", "-n", type=int, default=cpu_count(),
+    parser.add_argument("--nthreads", "-n", type=int, default=12,
                         help="number of threads")
     parser.add_argument("--channel", "-c", type=int, default=0,
                         help="channel to be converted")
     parser.add_argument("--dark", "-d", type=int, default=0,
                         help="background vs foreground threshold")
+    parser.add_argument("--convert_to_8bit", default=False, action=BooleanOptionalAction,
+                        help="convert to 8-bit")
     parser.add_argument("--bit_shift", "-b", type=int, default=8,
                         help="bit_shift for 8-bit conversion")
 
