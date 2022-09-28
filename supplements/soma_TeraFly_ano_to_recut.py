c8ec9b2da4b160ae12c47d0fa8bb8e2f299ca1a2
Keivan Moradi <k.moradi@gmail.com>
2022-09-27 22:17:56 -0700

    * delete soma_recut folder if it exists already

--- supplements/soma_TeraFly_ano_to_recut.py
+++ supplements/soma_TeraFly_ano_to_recut.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 from pandas import read_csv
+from shutil import rmtree
 
 
 # annotation file that contains soma locations only
-annotations = Path(r"/qnap/3D_stitched_LS/20220725_SW220510_02_LS_6x_1000z/Ex_488_Em_525_Terafly_Ano/Somata/SW220406_01_LS_6x_1000z_combined_stamp_2022_09_01_18_18.ano.apo")
+annotations = next(
+    Path(r"/data/20220725_17_41_55_SW220510_02_LS_6x_1000z_stitched").glob(
+        "SW220406_01_LS_6x_1000z_combined_stamp*.ano.apo"))
 recut = annotations.parent / 'soma_recut'
-recut.mkdir(exist_ok=True)
+if recut.exists():
+    rmtree(recut)
+recut.mkdir()
 annotations_df = read_csv(annotations)
-soma_diam = 1000
+soma_diam = 28
 for column in ("x", "y", "z", "volsize"):
     annotations_df[column] = annotations_df[column].round(decimals=0).astype(int)
 
