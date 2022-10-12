bc9235bbbb501fbae38ee567c96e9e66a0015c7a
Keivan Moradi <k.moradi@gmail.com>
2022-10-11 20:31:35 -0700

    * add radius to marker files

--- supplements/soma_TeraFly_ano_to_recut.py
+++ supplements/soma_TeraFly_ano_to_recut.py
@@ -1,22 +1,23 @@
 from pathlib import Path
 from pandas import read_csv
 from shutil import rmtree
+from math import pi
 
 
 # annotation file that contains soma locations only
 annotations = next(
-    Path(r"/data/20220725_17_41_55_SW220510_02_LS_6x_1000z_stitched").glob(
-        "SW220406_01_LS_6x_1000z_combined_stamp*.ano.apo"))
+    Path(r"/qnap/3D_stitched_LS/20220725_SW220510_02_LS_6x_1000z/Ex_488_Em_525_Terafly_Ano/Somata").glob(
+        "SW220510_02_LS_6x_1000z_combined_stamp*.ano.apo"))
 recut = annotations.parent / 'soma_recut'
 if recut.exists():
     rmtree(recut)
 recut.mkdir()
 annotations_df = read_csv(annotations)
-soma_diam = 28
+soma_radius_um = 8.0
 for column in ("x", "y", "z", "volsize"):
     annotations_df[column] = annotations_df[column].round(decimals=0).astype(int)
 
 for row in annotations_df.itertuples():
-    with open(recut/f"marker_{row.x}_{row.y}_{row.z}_{soma_diam}", 'w') as soma_file:  # row.volsize * 3
-        soma_file.write("# x,y,z\n")
-        soma_file.write(f"{row.x},{row.y},{row.z}")
+    with open(recut/f"marker_{row.x}_{row.y}_{row.z}_{int(4/3*pi * soma_radius_um**3 + 0.5)}", 'w') as soma_file:
+        soma_file.write("# x,y,z,radius_um\n")
+        soma_file.write(f"{row.x},{row.y},{row.z},{soma_radius_um}")
