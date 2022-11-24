36841b037beda8381a3375bc8bb27625d2f45337
Keivan Moradi <k.moradi@gmail.com>
2022-11-23 20:12:24 -0800

    * testing changes for the new microscope

--- LsDeconvolveMultiGPU/deconvolve.m
+++ LsDeconvolveMultiGPU/deconvolve.m
@@ -1,21 +1,21 @@
 LsDeconv(...
-    "/panfs/dong/U01_MORF/upload/20220818_22_59_23_Camk2a_MORF3_D1Tom_TME12_1_15x_5x5_x042_y042_z100_stitched/15x/Ex_561_Em_1_tif", ...
-    422, ... % dxy (nm)
-    1000, ... % dz (nm)
+    "/data/20221122_10_15_02_Camk2a_MORF3_Bcl11bKO_311_3_LH_15x_5x5_z04_2_5percent_stitched/Ex_647_Em_690_tif_bs=1", ...
+    400, ... % dxy (nm)
+    400, ... % dz (nm)
     9,    ... % numit
     0.40, ... % NA
     1.52, ... % rf
-    561,  ... % lambda_ex 488 561 642
-    600,  ... % lambda_em 525 600 680
-    240,  ... % fcyl
+    647,  ... % lambda_ex 488 561 642
+    690,  ... % lambda_em 525 600 680
+    80,  ... % fcyl 240 for the older unaligned stage
     12.0, ... % slitwidth (mm)
     0,    ... % damping percent or lambda
     0,    ... % clipval
     0,    ... % stop_criterion
-    6.9,  ... % block_size_max in GB
+    6.3,  ... % block_size_max in GB
     [1 2 3 4 5 6 7 8], ... % [1 2 3 4 5 6 7 8] gpu index in gpuDeviceTable, [0] means CPU
-    4.0,  ... % signal amplification if clipval=0. clipval=1 means no amplification.
-    [0.5, 0.5, 2.0], ... % x y z sigma of the 3D gaussian filter applied before deconvolution
+    3.0,  ... % signal amplification if clipval=0. clipval=1 means no amplification.
+    [0.5, 0.5, 0.5], ... % x y z sigma of the 3D gaussian filter applied before deconvolution [0.5, 0.5, 2.0]
     1,    ... % 0 not resume, 1 = resume
     1,    ... % starting block should be greater than 0 for multiGPU processing
     "/data/cache_deconvolution_yang/" ... % cache drive (optional)
