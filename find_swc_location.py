ddc7263ba9834ae8d4641a99b4fe09064340c4e8
Keivan Moradi <k.moradi@gmail.com>
2023-02-08 22:52:10 -0800

    bug fix

--- find_swc_location.py
+++ find_swc_location.py
@@ -1,30 +1,30 @@
-import pyvista
-import pymeshfix
+from pyvista import Plotter, PolyData
+from pyvista import read as read_mesh
 from typing import Tuple, List
 from pathlib import Path
-from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace, BooleanOptionalAction
+from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace
 from pandas import read_csv
 from shutil import copy
 
 
 def is_point_inside_surface(surface, points: List[Tuple[float, float, float]]):
-    points_poly = pyvista.PolyData(points)
+    points_poly = PolyData(points)
     select = points_poly.select_enclosed_points(surface, check_surface=False)
     return (p == 1 for p in select['SelectedPoints'])
 
 
 def convert_wrl_to_obj(wrl_file: Path, obj_file: Path):
-    pl = pyvista.Plotter()
+    pl = Plotter()
     pl.import_vrml(wrl_file.__str__())
     pl.export_obj(obj_file.__str__())
 
 
 def load_mesh(obj_file: Path):
-    return pyvista.read(obj_file.__str__())
+    return read_mesh(obj_file.__str__())
 
 
-def correct_point(point: Tuple[float, float, float], y_axis_length: float, z_axis_length: float):
-    return tuple((point[0], point[1] - y_axis_length, point[2] - z_axis_length))
+def correct_point(point: Tuple[float, float, float], y_axis_length: float):
+    return tuple((point[0], point[1] - y_axis_length, -point[2]))
 
 
 class DotDict(dict):
@@ -57,13 +57,13 @@ def get_surface_meshes(surfaces: Path):
             convert_wrl_to_obj(wrl_file, obj_file)
         surface_meshes += [DotDict({
             'file_name': obj_file.name,
-            'mesh': load_mesh(obj_file)
+            'mesh': load_mesh(obj_file).flip_z()
         })]
     return surface_meshes
 
 
 def main(args: Namespace):
-    soma_coordinates = get_soma_locations(Path(args.reconstructions), args.y_axis_length, args.z_axis_length)
+    soma_coordinates = get_soma_locations(Path(args.reconstructions), args.y_axis_length)
     soma_coordinates_list = [soma.point for soma in soma_coordinates]
     surface_meshes = get_surface_meshes(Path(args.surfaces))
     for surface in surface_meshes:
@@ -76,38 +76,20 @@ def main(args: Namespace):
                 surface_path.mkdir(exist_ok=True)
                 copy(soma.swc_path, surface_path)
 
-    # reconstructions = Path(r"X:\3D_stitched_LS\20220725_SW220510_02_LS_6x_1000z\Ex_488_Em_525_Terafly_Ano\HPC SWC")
-    # regions = Path(r"Y:/3D_stitched_LS/20220725_SW220510_02_LS_6x_1000z/registration/structures")
-    # region = regions / "structure_000000000463_surface.wrl"
-
-    # pl.add_axes_at_origin(line_width=2, labels_off=False, zlabel=None)
-    # pl.add_mesh(pyvista.Sphere(center=(0, 0, 0)), show_edges=True)  # x, y, z
-
+    # from pyvista import UniformGrid
+    # regions = Path(r"Y:\3D_stitched_LS\20220725_SW220510_02_LS_6x_1000z\registration\structures")
+    # region = regions / "structure_000000000255_surface.wrl"
+    #
+    # pl = Plotter()
+    # pl.import_vrml(region.__str__())
     # pl.add_mesh(
-    #     pyvista.UniformGrid(
-    #         origin=(2686.89, 5968.00 - 17557, 4098.89 - 8400),
+    #     UniformGrid(
+    #         origin=(7240.0, -8836.0, -3765.0),
     #         dimensions=(100, 100, 1),
     #         spacing=(10, 10, 10)
     #     ), show_edges=True)
     # pl.camera_position = 'xy'
     # pl.show()
-
-    # meshfix = pymeshfix.MeshFix(mesh)
-    # meshfix.repair(remove_smallest_components=False, joincomp=True, verbose=True)
-    # mesh = meshfix.mesh
-
-    # tin = pymeshfix.PyTMesh(mesh)
-    # v, f = tin.return_arrays()
-    # mesh = pymeshfix.MeshFix(v, f).mesh
-
-    # mesh.plot()
-
-    # (0.0, 0.0, 0.0),
-    # (7000.0, -10000.0, -6800.0),
-    # (2686.89, 5968.00 - 17557, 4098.89 - 8400)
-
-    # a = correct_point((2686.89, 5968.00, 4098.89), 17557, 8400)
-    # print(a)
 
 
 if __name__ == '__main__':
@@ -117,11 +99,9 @@ if __name__ == '__main__':
         epilog="Developed 2023 by Keivan Moradi at UCLA, Hongwei Dong Lab (B.R.A.I.N) \n"
     )
     parser.add_argument("--reconstructions", "-r", type=str, required=True,
-                        help="Path folder containing all swc or eswc files.")
+                        help="Path folder containing all swc files.")
     parser.add_argument("--surfaces", "-s", type=str, required=True,
                         help="Path folder containing all surface files.")
     parser.add_argument("--y_axis_length", "-y", type=float, default=0.0,
                         help="Y-axis length in voxels. Default is 0.")
-    parser.add_argument("--z_axis_length", "-z", type=float, default=0.0,
-                        help="Z-axis length in voxels. Default is 0.")
     main(parser.parse_args())
