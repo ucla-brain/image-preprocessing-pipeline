1b0e6ed7a4d6cf30e14f55ee7282d909a6f7951a
Keivan Moradi <k.moradi@gmail.com>
2023-02-27 21:18:36 -0800

    bug fix

--- supplements/soma_terafly_ano_to_recut_seed.py
+++ supplements/soma_terafly_ano_to_recut_seed.py
@@ -9,20 +9,20 @@ def main(args: Namespace):
     apo_file = Path(args.apo_file)
     annotations_df = read_csv(apo_file)
 
-    recut = apo_file.parent / apo_file.name
+    recut = apo_file.parent.parent / apo_file.name
+    print(recut.__str__())
     if recut.exists():
         rmtree(recut)
-    recut.mkdir()
+    recut.mkdir(exist_ok=True)
 
     soma_radius_um = args.default_radius if args.default_radius > 0 else None
     for column in ("x", "y", "z", "volsize"):
         annotations_df[column] = annotations_df[column].round(decimals=0).astype(int)
 
     for row in annotations_df.itertuples():
-        if soma_radius_um:
-            volume = round(4 / 3 * pi * soma_radius_um ** 3, 3)
-        else:
+        if not soma_radius_um:
             soma_radius_um = row.volsize**(1/3)*3/4/pi
+        volume = round(4 / 3 * pi * soma_radius_um ** 3, 3)
         with open(recut / f"marker_{row.x}_{row.y}_{row.z}_{int(volume)}", 'w') as marker_file:
             marker_file.write("# x,y,z,radius_um\n")
             marker_file.write(f"{row.x},{row.y},{row.z},{soma_radius_um}")
