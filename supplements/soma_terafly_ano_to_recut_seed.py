9fb31146e18fe906b25bf7f23a650e4a8c396121
Keivan Moradi <k.moradi@gmail.com>
2023-02-28 13:52:50 -0800

    bug fix

--- supplements/soma_terafly_ano_to_recut_seed.py
+++ supplements/soma_terafly_ano_to_recut_seed.py
@@ -19,12 +19,15 @@ def main(args: Namespace):
         annotations_df[column] = annotations_df[column].round(decimals=0).astype(int)
 
     for row in annotations_df.itertuples():
+        x = row.x * args.voxel_size_x
+        y = row.y * args.voxel_size_y
+        z = row.z * args.voxel_size_z
         if not soma_radius_um:
             soma_radius_um = row.volsize**(1/3)*3/4/pi
         volume = round(4 / 3 * pi * soma_radius_um ** 3, 3)
         with open(recut / f"marker_{row.x}_{row.y}_{row.z}_{int(volume)}", 'w') as marker_file:
             marker_file.write("# x,y,z,radius_um\n")
-            marker_file.write(f"{row.x},{row.y},{row.z},{soma_radius_um}")
+            marker_file.write(f"{x},{y},{z},{soma_radius_um}")
 
     print(f"files saved in {recut.__str__()}")
 
@@ -39,4 +42,10 @@ if __name__ == '__main__':
                         help="Marker (apo) file containing all seed locations.")
     parser.add_argument("--default_radius", "-r", type=float, default=0, required=False,
                         help="Default .")
+    parser.add_argument("--voxel_size_x", "-dx", type=float, default=1,
+                        help="Image voxel size on x-axis (µm).")
+    parser.add_argument("--voxel_size_y", "-dy", type=float, default=1,
+                        help="Image voxel size on y-axis (µm).")
+    parser.add_argument("--voxel_size_z", "-dz", type=float, default=1,
+                        help="Image voxel size on z-axis (µm).")
     main(parser.parse_args())
