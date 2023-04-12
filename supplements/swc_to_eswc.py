060e4177cd9c348f6eb0b523ce9bf42349133aa8
Keivan Moradi <k.moradi@gmail.com>
2023-04-12 13:10:56 -0700

    * xyz voxel sizes and axes flipping functionality.

--- supplements/swc_to_eswc.py
+++ supplements/swc_to_eswc.py
@@ -1,4 +1,4 @@
-from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace
+from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace, BooleanOptionalAction
 from pathlib import Path
 from pandas import read_csv
 
@@ -16,7 +16,19 @@ def main(args: Namespace):
             continue
 
         swc_df = read_csv(swc_file, sep=r"\s+", comment="#", index_col=0,
-                          names=("id", "type_id", "x", "y", "z", "radius", "parent_id"))
+                          names=("id", "type", "x", "y", "z", "radius", "parent_id"))
+
+        if args.x_axis_length > 0:
+            swc_df['x'] = args.x_axis_length - swc_df['x']
+        if args.y_axis_length > 0:
+            swc_df['y'] = args.y_axis_length - swc_df['y']
+        if args.z_axis_length > 0:
+            swc_df['z'] = args.z_axis_length - swc_df['z']
+
+        swc_df['x'] *= args.voxel_size_x
+        swc_df['y'] *= args.voxel_size_y
+        swc_df['z'] *= args.voxel_size_z
+
         for col_name, value in (("seg_id", 0), ("level", 1), ("mode", 0), ("timestamp", 1), ("TFresindex", 1)):
             swc_df[col_name] = value
         # print(swc_df.head())
@@ -42,4 +54,19 @@ if __name__ == '__main__':
     )
     parser.add_argument("--reconstructions", "-r", type=str, required=True,
                         help="Path folder containing all swc files.")
+    parser.add_argument("--voxel_size_x", "-dx", type=float, required=False, default=1.0,
+                        help="voxel size on the x-axis. Default value is 1, i.e. no for voxel size.")
+    parser.add_argument("--voxel_size_y", "-dy", type=float, required=False, default=1.0,
+                        help="voxel size on the y-axis. Default value is 1, i.e. no for voxel size.")
+    parser.add_argument("--voxel_size_z", "-dz", type=float, required=False, default=1.0,
+                        help="voxel size on the z-axis. Default value is 1, i.e. no for voxel size.")
+    parser.add_argument("--x_axis_length", "-x", type=int, required=False, default=0,
+                        help="The length of x-axis in pixels. "
+                             "If x>0 is provided x-axis will be flipped. Default is 0 --> no x-axis flipping")
+    parser.add_argument("--y_axis_length", "-y", type=int, required=False, default=0,
+                        help="The length of y-axis in pixels. "
+                             "If y>0 is provided y-axis will be flipped. Default is 0 --> no y-axis flipping")
+    parser.add_argument("--z_axis_length", "-z", type=int, required=False, default=0,
+                        help="The length of z-axis in pixels. "
+                             "If z>0 is provided z-axis will be flipped. Default is 0 --> no z-axis flipping")
     main(parser.parse_args())
