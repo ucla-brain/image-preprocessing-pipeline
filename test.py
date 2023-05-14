a2d2cdaca91342db6ac5aacc806771a8790bcdd3
Keivan Moradi <k.moradi@gmail.com>
2023-05-13 20:00:53 -0700

    * minor bug fixes

--- test.py
+++ test.py
@@ -25,9 +25,9 @@ def main():
 
 
 if __name__ == '__main__':
-    os.environ['MKL_NUM_THREADS'] = '1'
-    os.environ['NUMEXPR_NUM_THREADS'] = '1'
-    os.environ['OMP_NUM_THREADS'] = '1'
+    # os.environ['MKL_NUM_THREADS'] = '1'
+    # os.environ['NUMEXPR_NUM_THREADS'] = '1'
+    # os.environ['OMP_NUM_THREADS'] = '1'
     freeze_support()
     main()
 
