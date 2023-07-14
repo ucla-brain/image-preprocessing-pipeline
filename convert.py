f8c4b2cb27841b0313627fd49188341820f4aa45
Keivan Moradi <k.moradi@gmail.com>
2023-07-14 01:34:02 -0700

    bug fix for hugepage testing

--- convert.py
+++ convert.py
@@ -207,6 +207,7 @@ def main(args: Namespace):
 
 if __name__ == '__main__':
     freeze_support()
+    os.environ["GLIBC_TUNABLES"] = "glibc.malloc.hugetlb=2"
     cpu_instruction = "SSE2"
     for item in ["SSE2", "AVX", "AVX2", "AVX512f"]:
         cpu_instruction = item if CPUFeature[item] else cpu_instruction
