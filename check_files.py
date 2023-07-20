2b1eb9259e939729a03c1e6e07b9bf4893cfeb7f
Keivan Moradi <k.moradi@gmail.com>
2023-07-19 18:47:05 -0700

    optimization stuff

--- check_files.py
+++ check_files.py
@@ -2,22 +2,25 @@
 Read tif files and if the file was damaged print its name.
 """
 import sys
-from pathlib import Path
-from multiprocessing import freeze_support
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from multiprocessing import freeze_support
+from pathlib import Path
+from typing import List
+
+from psutil import cpu_count
+
 from pystripe.core import imread_tif_raw_png, glob_re
-from tsv.convert import calculate_cores_and_chunk_size
 from supplements.cli_interface import PrintColors
+from tsv.convert import calculate_cores_and_chunk_size
 
 
 def test_image(file: Path):
     try:
-        # if not isinstance(file, Path):
-        #     file = Path(file)
-        if file.is_file() and file.suffix == ".tif":
-            with ThreadPoolExecutor(1) as tp:
-                future = tp.submit(imread_tif_raw_png, file)
-                future.result(timeout=200)
+        if not isinstance(file, Path):
+            file = Path(file)
+        with ThreadPoolExecutor(1) as tp:
+            future = tp.submit(imread_tif_raw_png, file)
+            future.result(timeout=200)
     except Exception as e:
         print(f"{file} {type(e)} {e.args} {e}\n")
         file.unlink()
@@ -30,7 +33,10 @@ if __name__ == "__main__":
     else:
         print(f"{PrintColors.FAIL}Only one argument is allowed!{PrintColors.ENDC}")
         raise RuntimeError
-    with ProcessPoolExecutor(60) as pool:
-        files = list(glob_re(r"\.(?:tiff?|raw|png)$", source))
-        cores, chunks = calculate_cores_and_chunk_size(len(files), cores=60, pool_can_handle_more_than_61_cores=False)
+    files: List[Path] = list(glob_re(r"\.(?:tiff?|raw|png)$", source))
+    num_threads, chunks = calculate_cores_and_chunk_size(
+        len(files),
+        cores=cpu_count(logical=True),
+        pool_can_handle_more_than_61_cores=not sys.platform.lower() == 'win32')
+    with ProcessPoolExecutor(num_threads) as pool:
         list(pool.map(test_image, files, chunksize=chunks))
