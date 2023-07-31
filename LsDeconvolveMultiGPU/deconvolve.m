98e4bc4f04838a1a700d667c8954f2ae092919de
Keivan Moradi <k.moradi@gmail.com>
2023-07-31 16:53:27 -0700

    * ability to adjust denoising level

--- LsDeconvolveMultiGPU/deconvolve.m
+++ LsDeconvolveMultiGPU/deconvolve.m
@@ -18,7 +18,7 @@ LsDeconv(...
     [1, 1, 1.5], ... % x y z sigma of the 3D gaussian filter applied before deconvolution. filter_size = ceil(sigma * 4 + 1)
     [5, 5, 9], ... % filter_size
     20,   ... % denoising and background subtraction strength [0 to 255] for 8bit and [0 to 65535] for 16bit images
-    0,    ... % 0 not resume, 1 = resume
+    1,    ... % 0 not resume, 1 = resume
     1,    ... % starting block should be greater than 0 for multiGPU processing
     0,    ... % 1 flip the deconvolved image upside down. 0 do not.
     1,    ... % 1 convert_to_8bit, 0 keep as is
