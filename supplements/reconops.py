252ecae3f1774dd0338f5d19905ed3e7f4cd86aa
Keivan Moradi <k.moradi@gmail.com>
2023-10-17 15:27:19 -0700

    * bug fix

--- supplements/reconops.py
+++ supplements/reconops.py
@@ -171,7 +171,7 @@ def main(args: Namespace):
                 else:
                     output_folder = output_file.parent / output_file.name[0:-len(output_file.suffix)]
                 output_folder.mkdir(exist_ok=True)
-                for i in range(1, len(swc_df)):
+                for i in range(0, len(swc_df)):
                     df1: DataFrame = swc_df.iloc[[i]].copy()
                     output_file_new = output_folder / f"x{int(df1['x'])}-y{int(df1['y'])}-z{int(df1['z'])}.swc"
                     if is_overwrite_needed(output_file_new, args.overwrite):
