99c8e54d05c57792d5ad219a724ea98ee7757b07
Keivan Moradi <k.moradi@gmail.com>
2024-02-11 21:06:56 -0800

    * bug fix: bleach correction

--- process_images.py
+++ process_images.py
@@ -608,7 +608,7 @@ def process_channel(
             lb, ub = threshold_multiotsu(img, classes=3)
             assert isinstance(lb, float32)
             assert isinstance(ub, float32)
-            bit_shift = estimate_bit_shift(int(np_round(expm1(prctl(img[img > ub], 99.999)))))
+            bit_shift = estimate_bit_shift(int(np_round(expm1(prctl(img[img > lb], 99.999)))))
             background = int(np_round(expm1(lb)))
         return background, bit_shift
 
