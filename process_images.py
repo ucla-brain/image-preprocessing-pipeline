a630d698bf7bbb146c7b4db6b0cfd2b0d84c4b37
Keivan Moradi <k.moradi@gmail.com>
2024-02-24 00:53:27 -0800

    *  Memory management improvement

--- process_images.py
+++ process_images.py
@@ -634,7 +634,7 @@ def process_channel(
                 # frequency = 1 / sig
 
         sigma = (int(sig * 2), ) * 2
-        memory_needed_per_thread = 28 if need_bleach_correction else 16
+        memory_needed_per_thread = 24 if need_bleach_correction else 16
         memory_needed_per_thread *= shape[1] + 2 * calculate_pad_size(shape=shape[1:3], sigma=max(sigma)) + shape[1] % 2
         memory_needed_per_thread *= shape[2] + 2 * calculate_pad_size(shape=shape[1:3], sigma=max(sigma)) + shape[2] % 2
         memory_needed_per_thread /= 1024 ** 3
