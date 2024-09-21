eb2e379452b51e269fb22d0d705e2089c2b1ea2d
Keivan Moradi <k.moradi@gmail.com>
2024-09-20 19:41:13 -0700

    * fnt cube processor: deconvolution debug and performance enhancement

--- LsDeconvolveMultiGPU/deconvolve_win.m
+++ LsDeconvolveMultiGPU/deconvolve_win.m
@@ -1,7 +1,7 @@
 setenv("CUDA_VISIBLE_DEVICES","");
 LsDeconv(...
-    "X:\20230624_SM230223_05_LS_15x_800z_Double_MORF_B4\test", ...
-    400, ... % dxy (nm)
+    "E:\TMP\Keivan\LHA1\c0", ...
+    800, ... % dxy (nm)
     800, ... % dz (nm)
     25,   ... % numit
     0.40, ... % NA
@@ -16,12 +16,12 @@ LsDeconv(...
     intmax('int32'),... % block_size_max=max number of elements in the block. GPU arrays on MATLAB are limited to intmax('int32') as the max size.
     repmat(1:2, 1, 5),  ... % [repmat(1:2, 1, 4) zeros(1, 64)] gpu index in gpuDeviceTable, 0 means CPU
     1.0,  ... % signal amplification if clipval=0. clipval=1 means no amplification.
-    [1.0, 1.0, 1.5], ... % x y z sigma of the 3D gaussian filter applied before deconvolution. filter_size = ceil(sigma * 4 + 1)
-    [3, 3, 9], ... % filter_size
-    20,   ... % denoising and background subtraction strength [0 to 255] for 8bit and [0 to 65535] for 16bit images
+    [.5, .5, .5], ... % x y z sigma of the 3D gaussian filter applied before deconvolution. filter_size = ceil(sigma * 4 + 1)
+    [3, 3, 13], ... % filter_size
+    2,   ... % denoising and background subtraction strength [0 to 255] for 8bit and [0 to 65535] for 16bit images
     1,    ... % 0 not resume, 1 = resume
     1,    ... % starting block should be greater than 0 for multiGPU processing
     0,    ... % 1 flip the deconvolved image upside down. 0 do not.
     1,    ... % convert to 8-bit
-    "X:\cache_deconvolution_b4" ... % cache drive (optional)
+    "cache" ... % cache drive (optional)
 );
--- fnt_cube_processor.py
+++ fnt_cube_processor.py
@@ -1,70 +1,72 @@
 import os
 import sys
+import contextlib
 from argparse import RawDescriptionHelpFormatter, ArgumentParser, BooleanOptionalAction
 from multiprocessing import freeze_support, Queue
 from pathlib import Path
 from platform import uname
-from subprocess import call
 
-from nrrd import read, write
 import psutil
-from numpy import rot90, float32, iinfo, clip
+from nrrd import read, write
 from numpy import dtype as np_d_type
+from numpy import rot90, float32, iinfo, clip
 from psutil import cpu_count
+from pycudadecon import make_otf, decon
 from skimage.filters import gaussian
+from tifffile import imwrite
 from tqdm import tqdm
-
-from pystripe.core import (filter_streaks, is_uniform_2d, MultiProcessQueueRunner, progress_manager, cuda_device_count,
-                           cuda_get_device_properties, USE_PYTORCH)
+from shutil import copy
 
 from LsDeconvolveMultiGPU.psf_generation import generate_psf
-from pycudadecon import make_otf, decon
-from tifffile import imread, imwrite
+from pystripe.core import filter_streaks, is_uniform_2d, is_uniform_3d, MultiProcessQueueRunner, progress_manager
 
 
 def make_a_list_of_input_output_paths(args):
     input_folder = Path(args.input)
     output_folder = Path(args.output)
 
-    psf, dxy_psf, full_half_with_maxima_xy, full_half_with_maxima_z = generate_psf(
+    deconvolution_args = None
+    if args.deconvolution:
+        psf, dxy_psf = generate_psf(
             lambda_em=args.wavelength_em,
             lambda_ex=args.wavelength_ex,
+            numerical_aperture=args.na,
             dxy=args.dxy * 1000.0,
             dz=args.dz * 1000.0,
+            refractive_index=args.nimm,
             f_cylinder_lens=args.f_cylinder_lens,
             slit_width=args.slit_width,
         )
-    psf = rot90(psf, k=1, axes=(0, 2))
-    # print(output_folder)
-    psf_filepath = (output_folder / 'temp' / 'psf.tif').__str__()
-    # print(psf_filepath)
-    (output_folder / r"temp").mkdir(parents=True, exist_ok=True)
-    imwrite(psf_filepath, psf)
+        psf = rot90(psf, k=1, axes=(0, 2))
+        psf_filepath = (output_folder / 'temp' / 'psf.tif').__str__()
+        (output_folder / r"temp").mkdir(parents=True, exist_ok=True)
+        imwrite(psf_filepath, psf)
 
-    otf_file = make_otf(psf_filepath,
-                        outpath=(output_folder / 'temp' / 'otf.tif').__str__(),
-                        dzpsf=args.dz,
-                        dxpsf=dxy_psf / 1000.0,
-                        wavelength=args.wavelength_em,
-                        na=args.na,
-                        nimm=args.nimm
-                        )
-    # otf = imread(otf_file)
+        otf_file = make_otf(
+            psf_filepath,
+            outpath=(output_folder / 'temp' / 'otf.tif').__str__(),
+            dzpsf=args.dz,
+            dxpsf=dxy_psf / 1000.0,
+            wavelength=args.wavelength_em,
+            na=args.na,
+            nimm=args.nimm
+        )
+        # otf = imread(otf_file)
 
-    print(f"dxy_psf: {dxy_psf}")
+        print(f"dxy_psf: {dxy_psf}")
 
-    deconvolution_args = make_deconvolution_args(
-        otf_file,
-        n_iters=args.n_iters,
-        dz_data=args.dz,
-        dx_data=args.dxy,
-        dz_psf=args.dz,
-        dxy_psf=dxy_psf / 1000.0,
-        background=args.background,
-        wavelength_em=args.wavelength_em,
-        na=args.na,
-        nimm=args.nimm
-    )
+        deconvolution_args = make_deconvolution_args(
+            otf_file,
+            n_iters=args.n_iters,
+            dz_data=args.dz,
+            dx_data=args.dxy,
+            dz_psf=args.dz,
+            dxy_psf=dxy_psf / 1000.0,
+            background=args.background,
+            wavelength_em=args.wavelength_em,
+            na=args.na,
+            nimm=args.nimm
+        )
 
     def output_file(input_file: Path):
         return {
@@ -72,7 +74,6 @@ def make_a_list_of_input_output_paths(args):
             "output_file": (output_folder / input_file.relative_to(input_folder)),
             "need_destripe": args.destripe,
             "need_gaussian": args.gaussian,
-            "need_video": args.video,
             "need_deconvolution": args.deconvolution,
             "deconvolution_args": deconvolution_args
         }
@@ -109,12 +110,38 @@ def make_deconvolution_args(
     }
 
 
+@contextlib.contextmanager
+def suppress_output():
+    # Save the original file descriptors
+    original_stdout_fd = sys.stdout.fileno()
+    original_stderr_fd = sys.stderr.fileno()
+
+    # Duplicate the original file descriptors
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    saved_stderr_fd = os.dup(original_stderr_fd)
+
+    # Open /dev/null and redirect stdout and stderr to it
+    with open(os.devnull, 'w') as devnull:
+        os.dup2(devnull.fileno(), original_stdout_fd)
+        os.dup2(devnull.fileno(), original_stderr_fd)
+
+    try:
+        yield
+    finally:
+        # Restore the original file descriptors
+        os.dup2(saved_stdout_fd, original_stdout_fd)
+        os.dup2(saved_stderr_fd, original_stderr_fd)
+
+        # Close the duplicated file descriptors
+        os.close(saved_stdout_fd)
+        os.close(saved_stderr_fd)
+
+
 def process_cube(
         input_file: Path,
         output_file: Path,
         need_destripe: bool = False,
         need_gaussian: bool = False,
-        need_video: bool = False,
         need_deconvolution: bool = False,
         deconvolution_args: dict = None,
 ):
@@ -124,67 +151,66 @@ def process_cube(
         if need_destripe or need_gaussian or need_deconvolution:
             img, header = read(input_file.__str__())
             dtype = img.dtype
-            if need_destripe:
-                img = rot90(img, k=1, axes=(1, 2))
-                for idx in range(0, img.shape[0], 1):
-                    if not is_uniform_2d(img[idx]):
-                        img[idx] = filter_streaks(img[idx], sigma=(1, 1), wavelet="db9", bidirectional=True)
-                img = rot90(img, k=-1, axes=(1, 2))
-            if need_gaussian:
-                if img.dtype != float32:
-                    img = img.astype(float32)
-                gaussian(img, 1, output=img)
-                img = img.astype(dtype)
-            if need_deconvolution:
-                if deconvolution_args is None:
-                    print("deconvolution arguments are not provided for deconvolution; skipping this step...")
-                else:
-                    # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-                    img = decon(
-                        img,
-                        deconvolution_args['otf'],  # '/mnt/md0/psf_ex642_em680.tif',  #
-                        fpattern=None,
-                        n_iters=deconvolution_args['n_iters'],  # int: Number of iterations, by default 10
-                        # fpattern='*.tif',  # str: used to filter files in a directory, by default "*.tif"
-                        dzdata=deconvolution_args['dz_data'],  # float: Z-step size of data, by default 0.5 um
-                        dxdata=deconvolution_args['dx_data'],  # float: XY pixel size of data, by default 0.1 um
-                        dzpsf=deconvolution_args['dz_psf'],  # float: Z-step size of the OTF, by default 0.1 um
-                        dxpsf=deconvolution_args['dxy_psf'],  # float: XY pixel size of the OTF, by default 0.1 um
-                        background=deconvolution_args['background'],  # int or 'auto': User-supplied background to subtract.
-                        # If 'auto', the median value of the last Z plane will be used as background. by default 80
-                        # rotate=0.0,
-                        # # float: Rotation angle; if not 0.0 then rotation will be performed around Y axis after deconvolution, by default 0
-                        # deskew=0.0,
-                        # # float: Deskew angle. If not 0.0 then deskewing will be performed before deconvolution, by default 0
-                        # width=0,  # int: If deskewed, the output image's width, by default 0 (do not crop)
-                        # shift=0,  # int: If deskewed, the output image's extra shift in X (positive->left), by default 0
-                        # pad_val=0.0,  # float: Value with which to pad image when deskewing, by default 0.0
-                        # save_deskewed=False,  # bool: Save deskewed raw data as well as deconvolution result, by default False
-                        napodize=8,  # int: Number of pixels to soften edge with, by default 15
-                        # nz_blend=27,  # int: Number of top and bottom sections to blend in to reduce axial ringing, by default 0
-                        # dup_rev_z=True,  # bool: Duplicate reversed stack prior to decon to reduce axial ringing, by default False
 
-                        wavelength=deconvolution_args['wavelength_em'],  # 642 int: Emission wavelength in nm (default: {520})
-                        na=deconvolution_args['na'],  # float: Numerical Aperture (default: {1.5})
-                        nimm=deconvolution_args['nimm'],  # float: Refractive index of immersion medium (default: {1.3})
-                        # otf_bgrd=None,  # int, None: Background to subtract. "None" = autodetect. (default: {None})
-                        # krmax=0,
-                        # # int: Pixels outside this limit will be zeroed (overwriting estimated value from NA and NIMM) (default: {0})
-                        # fixorigin=8,
-                        # # int: for all kz, extrapolate using pixels kr=1 to this pixel to get value for kr=0 (default: {10})
-                        # cleanup_otf=True,  # bool: Clean-up outside OTF support (default: {False})
-                        # max_otf_size=60000,  # int: Make sure OTF is smaller than this many bytes.
-                        # # Deconvolution may fail if the OTF is larger than 60KB (default: 60000)
-                    )
-                    if img.dtype != dtype and np_d_type(dtype).kind in ("u", "i"):
-                        clip(img, iinfo(dtype).min, iinfo(dtype).max, out=img)
-                        img = img.astype(dtype)
+            if is_uniform_3d(img):
+                copy(input_file, output_file)
+            else:
+                if need_gaussian:
+                    if img.dtype != float32:
+                        img = img.astype(float32)
+                    gaussian(img, .5, output=img)
 
-            write(filename=output_file.__str__(), data=img, header=header, compression_level=1)
-            if need_video:
-                input_file = output_file
-        if need_video:
-            return_code = call(f"{fnt_cube2video} {input_file} {output_file}", shell=True)
+                if need_destripe:
+                    img = rot90(img, k=1, axes=(1, 2))
+                    for idx in range(0, img.shape[0], 1):
+                        if not is_uniform_2d(img[idx]):
+                            img[idx] = filter_streaks(img[idx], sigma=(1, 1), wavelet="db9", bidirectional=True)
+                    img = rot90(img, k=-1, axes=(1, 2))
+
+                if need_deconvolution and deconvolution_args is not None:
+                    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+                    if img.dtype != float32:
+                        img = img.astype(float32)
+                    with suppress_output():
+                        img = decon(
+                            img,
+                            deconvolution_args['otf'],  # '/mnt/md0/psf_ex642_em680.tif',  #
+                            fpattern=None,  # str: used to filter files in a directory, by default "*.tif"
+                            n_iters=deconvolution_args['n_iters'],  # int: Number of iterations, by default 10
+                            dzdata=deconvolution_args['dz_data'],  # float: Z-step size of data, by default 0.5 um
+                            dxdata=deconvolution_args['dx_data'],  # float: XY pixel size of data, by default 0.1 um
+                            dzpsf=deconvolution_args['dz_psf'],  # float: Z-step size of the OTF, by default 0.1 um
+                            dxpsf=deconvolution_args['dxy_psf'],  # float: XY pixel size of the OTF, by default 0.1 um
+                            background=deconvolution_args['background'], # int or 'auto': User-supplied background to subtract.
+                            # If 'auto', the median value of the last Z plane will be used as background. by default 80
+                            # rotate=0.0,
+                            # # float: Rotation angle; if not 0.0 then rotation will be performed around Y axis after deconvolution, by default 0
+                            # deskew=0.0,
+                            # # float: Deskew angle. If not 0.0 then deskewing will be performed before deconvolution, by default 0
+                            # width=0,  # int: If deskewed, the output image's width, by default 0 (do not crop)
+                            # shift=0,  # int: If deskewed, the output image's extra shift in X (positive->left), by default 0
+                            # pad_val=0.0,  # float: Value with which to pad image when deskewing, by default 0.0
+                            # save_deskewed=False,  # bool: Save deskewed raw data as well as deconvolution result, by default False
+                            napodize=8,  # int: Number of pixels to soften edge with, by default 15
+                            # nz_blend=27,  # int: Number of top and bottom sections to blend in to reduce axial ringing, by default 0
+                            # dup_rev_z=True,  # bool: Duplicate reversed stack prior to decon to reduce axial ringing, by default False
+
+                            wavelength=deconvolution_args['wavelength_em'],  # 642 int: Emission wavelength in nm (default: {520})
+                            na=deconvolution_args['na'],  # float: Numerical Aperture (default: {1.5})
+                            nimm=deconvolution_args['nimm'],  # float: Refractive index of immersion medium (default: {1.3})
+                            # otf_bgrd=None,  # int, None: Background to subtract. "None" = autodetect. (default: {None})
+                            # krmax=0,  # int: Pixels outside this limit will be zeroed (overwriting estimated value from NA and NIMM) (default: {0})
+                            # fixorigin=8,
+                            # # int: for all kz, extrapolate using pixels kr=1 to this pixel to get value for kr=0 (default: {10})
+                            # cleanup_otf=True,  # bool: Clean-up outside OTF support (default: {False})
+                            # max_otf_size=60000,  # int: Make sure OTF is smaller than this many bytes.
+                            # # Deconvolution may fail if the OTF is larger than 60KB (default: 60000)
+                        )
+
+                if img.dtype != dtype and np_d_type(dtype).kind in ("u", "i"):
+                    clip(img, iinfo(dtype).min, iinfo(dtype).max, out=img)
+                    img = img.astype(dtype)
+                write(file=output_file.__str__(), data=img, header=header, compression_level=1)
     return return_code
 
 
@@ -223,13 +249,11 @@ def main(args):
 if __name__ == '__main__':
     freeze_support()
     if sys.platform == "win32":
-        fnt_cube2video = Path(r".") / "fnt" / "Windows" / "fnt-cube2video.exe"
         psutil.Process().nice(getattr(psutil, "IDLE_PRIORITY_CLASS"))
         os.environ['MKL_NUM_THREADS'] = '1'
         os.environ['NUMEXPR_NUM_THREADS'] = '1'
         os.environ['OMP_NUM_THREADS'] = '1'
     elif sys.platform == 'linux' and 'microsoft' not in uname().release.lower():
-        fnt_cube2video = Path(r".") / "fnt" / "Linux" / "fnt-cube2video"
         psutil.Process().nice(value=19)
         os.environ["NUMPY_MADVISE_HUGEPAGE"] = "1"
         os.environ["TERM"] = "xterm"
@@ -249,15 +273,13 @@ if __name__ == '__main__':
                         default=cpu_count(logical=False) + 4,
                         help="Number of CPU cores.")
     parser.add_argument("--destripe", default=True, action=BooleanOptionalAction,
-                        help="Destripe the image by default. Disable by --no_destripe.")
+                        help="Destripe the image by default. Disable by --no-destripe.")
     parser.add_argument("--gaussian", "-g", default=False, action=BooleanOptionalAction,
-                        help="Apply a 3D gaussian filter after destriping. Default is --no_gaussian.")
+                        help="Apply a 3D gaussian filter after destriping. Default is --no-gaussian.")
     parser.add_argument("--deconvolution", "-d", default=False, action=BooleanOptionalAction,
-                        help="Apply a deconvolution after destriping.  Default is --no_deconvolution.")
-    parser.add_argument("--video", "-v", default=False, action=BooleanOptionalAction,
-                        help="Convert cubes to video format. Default is --no_video.")
+                        help="Apply a deconvolution after destriping.  Default is --no-deconvolution.")
     parser.add_argument("--use_gpu", default=False, action=BooleanOptionalAction,
-                        help="Use gpu acceleration for destriping. Default is --no_use_gpu.")
+                        help="Use gpu acceleration for destriping. Default is --no-use-gpu.")
     parser.add_argument("--exclude_gpus", default=False,  # action=List[int],
                         help="Exclude GPUs during deconvolution.")
     parser.add_argument("--threads_per_gpu", type=int, required=False,
@@ -285,8 +307,8 @@ if __name__ == '__main__':
                         default=0.40,
                         help="Numerical Aperture, by default 0.4")
     parser.add_argument("--nimm", type=float, required=False,
-                        default=1.52,
-                        help="Refractive index of immersion medium, by default 1.52")
+                        default=1.42,
+                        help="Refractive index of immersion medium, by default 1.42")
     parser.add_argument("--background", type=float, required=False,
                         default=0,
                         help="int or 'auto': User-supplied background to subtract")
