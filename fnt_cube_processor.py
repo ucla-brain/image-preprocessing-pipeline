f4305c963748b346611fd3bc81bf854ae964c1b6
Keivan Moradi <k.moradi@gmail.com>
2025-04-03 15:22:37 -0700

    * Fixed the issue of running the code on Linux

--- fnt_cube_processor.py
+++ fnt_cube_processor.py
@@ -341,7 +341,7 @@ if __name__ == '__main__':
         raise RuntimeError
 
     num_gpus = str(check_output([nvidia_smi, "-L"])).count('UUID')
-    os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, range(num_gpus)))
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{','.join(map(str, range(num_gpus)))}"
     parser = ArgumentParser(
         description="Process FNT cubes in parallel\n\n",
         formatter_class=RawDescriptionHelpFormatter,
