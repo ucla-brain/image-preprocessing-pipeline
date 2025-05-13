ac63cd5bef9b82a35d2fe67f7a2b413f885a1948
Keivan Moradi <k.moradi@gmail.com>
2025-05-12 22:12:33 -0700

    * remove corrupt files parallel

--- LsDeconvolveMultiGPU/remove_corrupted_mat_files.m
+++ LsDeconvolveMultiGPU/remove_corrupted_mat_files.m
@@ -1,25 +1,22 @@
+% === Script: remove_corrupted_mat_files_parallel.m ===
 
-% === Script: remove_corrupted_mat_files.m ===
+% Specify folder
+folder_path = 'U:\tif\ADT5_9x_AAV_ds\cache_deconvolution_ADT5_Ex_561_Em_600';
 
-% Specify folder containing .mat files
-folder_path = 'V:\tif\ADT5_9x_AAV_ds\cache_deconvolution_ADT5_Ex_561_Em_600';  % <-- change this path
+% Get sorted list of .mat files
+mat_structs = natsortfiles(dir(fullfile(folder_path, '*.mat')));
+file_names = {mat_structs.name};
 
-% Get list of all .mat files in the folder
-mat_files = natsortfiles(dir(fullfile(folder_path, '*.mat')));
-
-% Loop through each file
-for k = 894:length(mat_files)
-    file_name = mat_files(k).name;
+% Use parallel loop
+parfor k = 1:length(file_names)
+    file_name = file_names{k};
     full_path = fullfile(folder_path, file_name);
-
-    fprintf('Checking file: %s\n', file_name);
-
     try
-        % Try loading the .mat file
-        load(full_path, '-mat');
-    catch ME
-        % If there's an error, assume the file is corrupted
-        fprintf('Corrupted file detected and deleted: %s\n', file_name);
+        % Load to a struct instead of workspace
+        S = load(full_path, '-mat');
+    catch
+        % Use warning or simple log
+        warning('Deleting corrupted file: %s\n', file_name);
         delete(full_path);
     end
 end
