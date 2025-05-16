1c4ae8c9284b56514382c4f456e57c183952ac81
Keivan Moradi <k.moradi@gmail.com>
2025-05-15 23:47:44 -0700

    * Build queue mex

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -6,18 +6,19 @@
 % Set source files
 src_semaphore = 'semaphore.c';
 src_queue = 'queue.c';
-src_wavedec2 = 'wavedec2_mex.cu';
-src_waverec2 = 'waverec2_mex.cu';
-src_filter_subband = 'filter_subband_mex.cu';
-build_xml = "C:/Program Files/MATLAB/R2023a/toolbox/parallel/gpu/extern/src/mex/win64/nvcc_msvcpp2022.xml";
+
+%src_wavedec2 = 'wavedec2_mex.cu';
+%src_waverec2 = 'waverec2_mex.cu';
+%src_filter_subband = 'filter_subband_mex.cu';
+%build_xml = "C:/Program Files/MATLAB/R2023a/toolbox/parallel/gpu/extern/src/mex/win64/nvcc_msvcpp2022.xml";
 
 % Include directory for headers
-root_dir = '.';
-include_dir = './cuda_kernels';
-setenv('CUDA_PATH', 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8');
-setenv('CUDA_ROOT', getenv('CUDA_PATH'));
-setenv('CUDA_TOOLKIT_ROOT_DIR', getenv('CUDA_PATH'));
-setenv('MW_ALLOW_ANY_CUDA', '1');
+%root_dir = '.';
+%include_dir = './cuda_kernels';
+%setenv('CUDA_PATH', 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8');
+%setenv('CUDA_ROOT', getenv('CUDA_PATH'));
+%setenv('CUDA_TOOLKIT_ROOT_DIR', getenv('CUDA_PATH'));
+%setenv('MW_ALLOW_ANY_CUDA', '1');
 
 
 % Compile
