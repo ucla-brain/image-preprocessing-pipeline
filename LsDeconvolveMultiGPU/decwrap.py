56136ba2bd375968a4741e61faf58b4a4a990740
Keivan Moradi <k.moradi@gmail.com>
2025-05-22 10:46:15 -0700

    Parameter tuning

--- LsDeconvolveMultiGPU/decwrap.py
+++ LsDeconvolveMultiGPU/decwrap.py
@@ -178,7 +178,7 @@ def main():
     # Optional
     parser.add_argument('--cache_drive', type=str, default=None,
                         help='Optional brain name for cache path construction')
-    parser.add_argument('--numit', '-it', type=int, default=8,
+    parser.add_argument('--numit', '-it', type=int, default=6,
                         help='Number of deconvolution iterations [1-50]')
     parser.add_argument('--na', type=float, default=0.40,
                         help='Numerical aperture of the objective lens')
@@ -222,7 +222,7 @@ def main():
                         help='Denoising strength (e.g., 1 to 255 for 8-bit images)')
     parser.add_argument('--destripe_sigma', type=float, default=0.0, # 0.125
                         help='Sigma of destriping filter along the z-axis. Use 0 to disable filtering.')
-    parser.add_argument('--regularize_interval', type=int, default=4,
+    parser.add_argument('--regularize_interval', type=int, default=3,
                         help='Apply a 3D Gaussian smoothing filter (σ=0.5) to the deconvolved volume every N iterations. '
                              'Enables blind deconvolution by updating the PSF after each smoothing step. '
                              'Set to 0 to disable both smoothing and blind deconvolution.')
