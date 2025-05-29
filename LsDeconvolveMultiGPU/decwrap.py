616674b4279ebd71528d9f5b09c61c54e5fbdb0b
Keivan Moradi <k.moradi@gmail.com>
2025-05-28 23:36:52 -0700

    parameter tuning

--- LsDeconvolveMultiGPU/decwrap.py
+++ LsDeconvolveMultiGPU/decwrap.py
@@ -265,7 +265,7 @@ def main():
         args.block_size_max = estimate_block_size_max(
             args.gpu_indices,
             args.gpu_workers_per_gpu * len(args.gpu_indices),
-            base_reserve_gb=4.5 if args.use_fft else 3.5,
+            base_reserve_gb=5 if args.use_fft else 3.5,
             num_blocks_on_gpu=(8 if args.use_fft else 2) + (1 if args.lambda_damping else 0),
         )
         log.info(f"Re-estimated block_size_max: {args.block_size_max}")
