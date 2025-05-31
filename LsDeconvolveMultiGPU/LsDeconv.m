567b7655e2e7f5d8f21f0853379e627acea29707
Keivan Moradi <k.moradi@gmail.com>
2025-05-31 08:23:03 -0700

    parameter tuning

--- LsDeconvolveMultiGPU/LsDeconv.m
+++ LsDeconvolveMultiGPU/LsDeconv.m
@@ -456,7 +456,7 @@ function process(inpath, outpath, log_file, stack_info, block, psf, numit, ...
     queue('create', gpu_queue_key, unique_gpus);
     for gpu = unique_gpus
         % semaphore_create(gpu, 1);
-        semaphore_create(gpu + semkey_loading_base, 6);
+        semaphore_create(gpu + semkey_loading_base, 10);
     end
 
     % start deconvolution
