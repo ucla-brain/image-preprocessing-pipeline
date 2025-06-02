0a9dffb28c2ac262a26f0aae7016b27d01f44bc7
Keivan Moradi <k.moradi@gmail.com>
2025-06-02 14:27:25 -0700

    parameter tuning

--- LsDeconvolveMultiGPU/otf_gpu_test.m
+++ LsDeconvolveMultiGPU/otf_gpu_test.m
@@ -3,7 +3,7 @@ fprintf('\n');
 fprintf('PF   Test  Type    Size              Sigma         Kernel          maxErr    RMS       relErr    mex(s)   Speedup\n');
 fprintf('---------------------------------------------------------------------------------------------------------------\n');
 
-sz = [500 500 500];
+sz = [400 400 400];
 kernels = {'auto', 9, [9 11 15], 3, 41};
 sigmas = {2.5, [2.5 2.5 2.5], [0.5 0.5 2.5], 0.25, 8};
 results = [];
