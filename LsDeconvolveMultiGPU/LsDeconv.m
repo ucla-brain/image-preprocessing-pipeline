112cf4469d8c84616b4bdd007d92ddd32dd27833
Keivan Moradi <k.moradi@gmail.com>
2025-06-02 10:07:39 -0700

    parameter tuning

--- LsDeconvolveMultiGPU/LsDeconv.m
+++ LsDeconvolveMultiGPU/LsDeconv.m
@@ -366,7 +366,7 @@ function pad_size = gaussian_pad_size(image_size, sigma, kernel)
 end
 
 function pad = decon_pad_size(psf_sz)
-    pad = ceil(psf_sz(:).' * (6/4));
+    pad = ceil(psf_sz(:).' * 2);
 end
 
 function n_vec = next_fast_len(n_vec)
