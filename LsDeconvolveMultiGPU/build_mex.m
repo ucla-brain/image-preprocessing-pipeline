9bdb200eb32793df9d00d830d8d8da21828c6111
Keivan Moradi <k.moradi@gmail.com>
2025-06-04 00:45:52 -0700

    compile cuda in windows

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -5,17 +5,6 @@
 % Downloads lz4.c/.h from GitHub if missing.
 % Requires MATLAB R2018a+ (-R2018a mxArray API).
 
-% --- PATCH: Force mexcuda -setup to use local nvcc_msvcpp2022.xml on Windows ---
-if ispc
-    this_xml = fullfile(fileparts(mfilename('fullpath')), 'nvcc_msvcpp2022.xml');
-    assert(isfile(this_xml), 'nvcc_msvcpp2022.xml not found!');
-    fprintf('Please run the following command ONCE in MATLAB to register your custom CUDA config:\n');
-    fprintf('  mexcuda -setup "%s"\n', this_xml);
-    % Or try interactive selection:
-    mexcuda('-setup');
-    % ...user picks the config from menu
-end
-
 debug = false;
 if verLessThan('matlab', '9.4')
     error('This script requires MATLAB R2018a or newer (for -R2018a MEX API)');
@@ -90,9 +79,18 @@ end
 % CUDA include dirs (if any)
 root_dir = '.'; include_dir = './mex_incubator';
 
+% Patch: Use custom XML for mexcuda only on Windows
+if ispc
+    xmlfile = fullfile(fileparts(mfilename('fullpath')), 'nvcc_msvcpp2022.xml');
+    assert(isfile(xmlfile), 'nvcc_msvcpp2022.xml not found!');
+    cuda_mex_flags = {'-f', xmlfile};
+else
+    cuda_mex_flags = {};
+end
+
 % Build CUDA Gaussian 3D MEX file (GPU)
-mexcuda(mex_flags{:}, src_gauss3d, ['-I', root_dir], ['-I', include_dir], nvccflags);
-mexcuda(mex_flags{:}, src_conv3d , ['-I', root_dir], ['-I', include_dir], nvccflags);
-mexcuda(mex_flags{:}, src_otf_gpu, ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
+mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_gauss3d, ['-I', root_dir], ['-I', include_dir], nvccflags);
+mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_conv3d , ['-I', root_dir], ['-I', include_dir], nvccflags);
+mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_otf_gpu, ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
 
 fprintf('All MEX files built successfully.\n');
