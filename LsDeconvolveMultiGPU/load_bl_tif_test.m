ee642fbb05f8f10a2059d0b40123f20c3d6a7f8d
Keivan Moradi <k.moradi@gmail.com>
2025-06-06 13:53:09 -0700

    bug fix

--- LsDeconvolveMultiGPU/load_bl_tif_test.m
+++ LsDeconvolveMultiGPU/load_bl_tif_test.m
@@ -48,7 +48,7 @@ function load_bl_tif_test()
 
             % MATLAB reference
             t1 = tic;
-            bl_gt = zeros(blkW, blkH, numel(z_indices), 'uint16');  % MATLAB expects [W, H, Z]
+            bl_gt = zeros(blkH, blkW, numel(z_indices), 'uint16');  % MATLAB expects [W, H, Z]
             for k = 1:numel(z_indices)
                 slice = imread(filelist{z_indices(k)}, ...
                     'PixelRegion', {[y_indices(1), y_indices(end)], [x_indices(1), x_indices(end)]});
