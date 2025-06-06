5c7d5654fcc2b6b3044f4894af7d28d1683d688a
Keivan Moradi <k.moradi@gmail.com>
2025-06-06 14:57:48 -0700

    bug fix

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -38,24 +38,6 @@ if ~isfile('lz4.h')
     try, websave('lz4.h', lz4_h_url); catch, error('Failed to download lz4.h'); end
 end
 
-% MEX optimization flags (CPU)
-mex_flags = {'-R2018a'};
-if debug
-    if ispc
-        mex_flags_cpu = [mex_flags, {'COMPFLAGS="$COMPFLAGS /Od /Zi /openmp"'}];
-    else
-        mex_flags_cpu = [mex_flags, {'CFLAGS="$CFLAGS -O0 -g -fopenmp"', ...
-                                     'CXXFLAGS="$CXXFLAGS -O0 -g -fopenmp"'}];
-    end
-else
-    if ispc
-        mex_flags_cpu = [mex_flags, {'COMPFLAGS="$COMPFLAGS /O2 /arch:AVX2 /openmp"'}];
-    else
-        mex_flags_cpu = [mex_flags, {'CFLAGS="$CFLAGS -O3 -march=native -fomit-frame-pointer -fopenmp"', ...
-                                     'CXXFLAGS="$CXXFLAGS -O3 -march=native -fomit-frame-pointer -fopenmp"'}];
-    end
-end
-
 % Use Anaconda-provided libtiff for all platforms
 conda_prefix = getenv('CONDA_PREFIX');
 assert(~isempty(conda_prefix) && isfolder(conda_prefix), ...
@@ -63,26 +45,60 @@ assert(~isempty(conda_prefix) && isfolder(conda_prefix), ...
 
 tiff_include = {['-I', fullfile(conda_prefix, 'include')]};
 tiff_lib     = {['-L', fullfile(conda_prefix, 'lib')]};
-tiff_link    = {'-ltiff'};
+
+% --- Platform-specific linking and flags ---
+if ispc
+    % On Windows, typically need libtiff.lib
+    tiff_libfile = fullfile(conda_prefix, 'lib', 'libtiff.lib');
+    if ~isfile(tiff_libfile)
+        tiff_libfile = fullfile(conda_prefix, 'lib', 'tiff.lib');
+        if ~isfile(tiff_libfile)
+            error(['Neither libtiff.lib nor tiff.lib found in: ' fullfile(conda_prefix, 'lib')]);
+        end
+    end
+    tiff_link = {tiff_libfile};
+
+    if debug
+        mex_flags_cpu = {'-R2018a', 'COMPFLAGS="$COMPFLAGS /Od /Zi /openmp"'};
+    else
+        mex_flags_cpu = {'-R2018a', 'COMPFLAGS="$COMPFLAGS /O2 /arch:AVX2 /openmp"'};
+    end
+else
+    % On Linux/Mac, typically need libtiff.so
+    tiff_libfile = fullfile(conda_prefix, 'lib', 'libtiff.so');
+    if ~isfile(tiff_libfile)
+        error(['libtiff.so not found in: ' fullfile(conda_prefix, 'lib')]);
+    end
+    tiff_link = {'-ltiff'};
+
+    if debug
+        mex_flags_cpu = {'-R2018a', 'CFLAGS="$CFLAGS -O0 -g -fopenmp"', ...
+                         'CXXFLAGS="$CXXFLAGS -O0 -g -fopenmp"'};
+    else
+        mex_flags_cpu = {'-R2018a', ...
+            'CFLAGS="$CFLAGS -O3 -march=native -fomit-frame-pointer -fopenmp"', ...
+            'CXXFLAGS="$CXXFLAGS -O3 -march=native -fomit-frame-pointer -fopenmp"'};
+    end
+end
 
 fprintf('Using Anaconda libtiff from: %s\n', conda_prefix);
 
 % Build CPU MEX files
-%mex(mex_flags_cpu{:}, src_semaphore);
-%mex(mex_flags_cpu{:}, src_lz4_save, src_lz4_c);
-%mex(mex_flags_cpu{:}, src_lz4_load, src_lz4_c);
+% mex(mex_flags_cpu{:}, src_semaphore);
+% mex(mex_flags_cpu{:}, src_lz4_save, src_lz4_c);
+% mex(mex_flags_cpu{:}, src_lz4_load, src_lz4_c);
 mex(mex_flags_cpu{:}, src_load_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:});
 
 % CUDA optimization flags
-if debug
-    if ispc
+if ispc
+    if debug
         nvccflags = 'NVCCFLAGS="$NVCCFLAGS -G -std=c++17 -Xcompiler ""/Od,/Zi"" "';
     else
-        nvccflags = 'NVCCFLAGS="$NVCCFLAGS -G -std=c++17 -Xcompiler ''-O0,-g'' "';
+        nvccflags = 'NVCCFLAGS="$NVCCFLAGS -O2 -std=c++17 -Xcompiler ""/O2,/arch:AVX2,/openmp"" "';
     end
 else
-    if ispc
-        nvccflags = 'NVCCFLAGS="$NVCCFLAGS -O2 -std=c++17 -Xcompiler ""/O2,/arch:AVX2,/openmp"" "';
+    if debug
+        nvccflags = 'NVCCFLAGS="$NVCCFLAGS -G -std=c++17 -Xcompiler ''-O0,-g'' "';
     else
         nvccflags = 'NVCCFLAGS="$NVCCFLAGS -O2 -std=c++17 -Xcompiler ''-O2,-march=native,-fomit-frame-pointer,-fopenmp'' "';
     end
@@ -101,9 +117,9 @@ else
 end
 
 % Build CUDA MEX files
-%mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_gauss3d , ['-I', root_dir], ['-I', include_dir], nvccflags);
-%mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_conv3d  , ['-I', root_dir], ['-I', include_dir], nvccflags);
-%mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_otf_gpu , ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
-%mexcuda(cuda_mex_flags{:}, mex_flags{:}, src_deconFFT, ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
+% mexcuda(cuda_mex_flags{:}, '-R2018a', src_gauss3d , ['-I', root_dir], ['-I', include_dir], nvccflags);
+% mexcuda(cuda_mex_flags{:}, '-R2018a', src_conv3d  , ['-I', root_dir], ['-I', include_dir], nvccflags);
+% mexcuda(cuda_mex_flags{:}, '-R2018a', src_otf_gpu , ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
+% mexcuda(cuda_mex_flags{:}, '-R2018a', src_deconFFT, ['-I', root_dir], ['-I', include_dir], nvccflags, '-L/usr/local/cuda/lib64', '-lcufft');
 
 fprintf('All MEX files built successfully.\n');
