047636f837766aad4c31352474ee61dfca0b4af9
Keivan Moradi <k.moradi@gmail.com>
2025-06-08 09:32:52 -0700

    testing link time optimization

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -180,8 +180,8 @@ function ok = try_build_library(lib, src_dir, install_dir, mex_flags_cpu)
             folder_name = 'jpeg-9e';
             url = 'https://ijg.org/files/jpegsrc.v9e.tar.gz';
         case 'zlib'
-            archive = 'zlib-1.3.tar.gz';
-            folder_name = 'zlib-1.3';
+            archive = 'zlib-1.3.1.tar.gz';
+            folder_name = 'zlib-1.3.1';
             url = 'https://zlib.net/zlib-1.3.tar.gz';
         case 'liblzma'
             archive = 'xz-5.4.2.tar.gz';
