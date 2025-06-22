b361fbfcb42e43de293e3c264919f4323aef3243
Keivan Moradi <k.moradi@gmail.com>
2025-06-22 08:49:09 -0700

    test performance improvement

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -100,9 +100,9 @@ mex(mex_flags_cpu{:}, src_lz4_load, src_lz4_c);
 mex(mex_flags_cpu{:}, src_load_slab_lz4, src_lz4_c);
 mex(mex_flags_cpu{:}, src_load_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:});
 if ispc
-    mex(mex_flags_cpu{:}, src_save_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:});
+    mex(mex_flags_cpu{:}, src_save_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:}, '-fopenmp');
 else
-    mex(mex_flags_cpu{:}, src_save_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:}, '-lnuma');
+    mex(mex_flags_cpu{:}, src_save_bl, tiff_include{:}, tiff_lib{:}, tiff_link{:}, '-lnuma', '-fopenmp');
 end
 
 % CUDA optimization flags
