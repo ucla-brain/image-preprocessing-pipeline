c53c8f9b13a70037ecfe646ff13e547d5b773e32
Keivan Moradi <k.moradi@gmail.com>
2025-06-29 20:50:02 -0700

    testing optimized Weiner update

--- LsDeconvolveMultiGPU/decon.m
+++ LsDeconvolveMultiGPU/decon.m
@@ -202,9 +202,6 @@ function bl = deconFFT_Weiner(bl, psf, fft_shape, niter, lambda, stop_criterion,
     otf_buff = complex(buff2, buff2);  % complex(single) zeros
 
     for i = 1:niter
-        [otf_buff, ~, ~] = pad_block_to_fft_shape(psf, fft_shape, 0);
-        otf_buff = ifftshift(otf_buff);
-        otf_buff = fftn(otf_buff);
 
         if i > 1 && regularize_interval>0 && mod(i, regularize_interval)==0
             if use_gpu, bl =  gauss3d_gpu(bl,0.5);
@@ -216,6 +213,10 @@ function bl = deconFFT_Weiner(bl, psf, fft_shape, niter, lambda, stop_criterion,
         % ----------- Richardson–Lucy core ------------
         % otf_buff = otf_gpu(psf, fft_shape, otf_buff);
         if i == 1
+            [otf_buff, ~, ~] = pad_block_to_fft_shape(psf, fft_shape, 0);
+            otf_buff = ifftshift(otf_buff);
+            otf_buff = fftn(otf_buff);
+
             buff1 = fftn(bl);                                            % F{Y}                                 complex
         end
         % convFFT start
@@ -260,13 +261,14 @@ function bl = deconFFT_Weiner(bl, psf, fft_shape, niter, lambda, stop_criterion,
             buff2 = buff1 .* buff3;                                      % F{X} . conj(F{X})                    real
             buff2 = max(buff2, single(eps('single')));                   % F{X} . conj(F{X}) + epsilon          real
             otf_buff = otf_buff ./ buff2;                                % otf_new                              complex
-            buff3 = ifftn(otf_buff);                                     % psf                                  complex
-            buff2 = real(buff3);                                         % psf                                  real
-            psf = buff2(center(1):center(1)+psf_sz(1)-1, ...
-                        center(2):center(2)+psf_sz(2)-1, ...
-                        center(3):center(3)+psf_sz(3)-1);
-            psf = max(psf, 0);            % clamp negatives
-            psf = psf / sum(psf(:));      % normalize to unit energy
+            otf_buff = otf_buff / otf_buff(1,1,1);                       % normalize to unit energy             complex
+            % buff3 = ifftn(otf_buff);                                     % psf                                  complex
+            % buff2 = real(buff3);                                         % psf                                  real
+            % psf = buff2(center(1):center(1)+psf_sz(1)-1, ...
+            %             center(2):center(2)+psf_sz(2)-1, ...
+            %             center(3):center(3)+psf_sz(3)-1);
+            % psf = max(psf, 0);            % clamp negatives
+            % psf = psf / sum(psf(:));      % normalize to unit energy
         end
 
         % ------------- stopping test -----------------
