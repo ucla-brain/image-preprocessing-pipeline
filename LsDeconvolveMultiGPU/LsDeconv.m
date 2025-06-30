c2658987c013fa036721f2822f7154323e30497f
Keivan Moradi <k.moradi@gmail.com>
2025-06-30 02:36:30 -0700

    more accurate logging

--- LsDeconvolveMultiGPU/LsDeconv.m
+++ LsDeconvolveMultiGPU/LsDeconv.m
@@ -943,11 +943,11 @@ function [bl, lb, ub] = process_block(bl, block, psf, niter, lambda, stop_criter
         reset(gpu_device);  % to free 2 extra copies of bl in gpu
         semaphore('p', semkey_gpu_base + gpu);
 
-        send( dQueue, sprintf('%s %.2f', logging_string, toc(block_processing_start)) );
+        send( dQueue, sprintf('%s %.1f', logging_string, toc(block_processing_start)) );
         [lb, ub] = deconvolved_stats(bl, clipval);
     else
         [lb, ub] = deconvolved_stats(bl, clipval);
-        send( dQueue, sprintf('%s %.2f', logging_string, toc(block_processing_start)) );
+        send( dQueue, sprintf('%s %.1f', logging_string, toc(block_processing_start)) );
     end
 
     assert(all(size(bl) == bl_size), '[process_block]: block size mismatch!');
