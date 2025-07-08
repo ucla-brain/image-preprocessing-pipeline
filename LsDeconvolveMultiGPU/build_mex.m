de208175e82a2a7d8cfa58ea5efe20f2810e0af7
Keivan Moradi <k.moradi@gmail.com>
2025-07-07 20:00:09 -0700

    thread affinity assigment

--- LsDeconvolveMultiGPU/build_mex.m
+++ LsDeconvolveMultiGPU/build_mex.m
@@ -226,7 +226,11 @@ function build_mex(debug)
     hwloc_v       = sprintf('%s.%s', hwloc_v_major, hwloc_v_patch);
     hwloc_src     = fullfile(thirdparty, ['hwloc-' hwloc_v]);
     hwloc_inst    = fullfile(build_root, 'hwloc');
-    hwloc_stamp = getStamp(hwloc_inst, iff(isWin, msvc.tag, ''));
+    if isWin
+        hwloc_stamp = getStamp(hwloc_inst, msvc.tag);
+    else
+        hwloc_stamp = getStamp(hwloc_inst, '');
+    end
 
     if ~isfile(hwloc_stamp)
         % Download hwloc source if missing
