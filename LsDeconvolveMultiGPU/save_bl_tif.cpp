6a4cbf12cf574b04808b76e871c381ef2a59d294
Keivan Moradi <k.moradi@gmail.com>
2025-07-13 03:33:18 -0700

    testing different version of save_bl_tif.cpp

--- LsDeconvolveMultiGPU/save_bl_tif.cpp
+++ LsDeconvolveMultiGPU/save_bl_tif.cpp
@@ -529,48 +529,53 @@ inline void fast_slice_copy(void* dst, const void* src, size_t bytes) {
     std::memcpy(dst, src, bytes);
 }
 
-// Thread-safe pool of preallocated buffers for each thread-pair
+// Lock-free, single producer, single consumer, circular buffer pool
 class BufferPool {
 public:
     BufferPool(unsigned numaNode, size_t bufferBytes, size_t poolSize, hwloc_topology_t topo)
-        : bufferBytes_(bufferBytes), topo_(topo)
+        : buffers_(poolSize, nullptr),
+          poolSize_(poolSize),
+          bufferBytes_(bufferBytes),
+          topo_(topo),
+          head_(0), tail_(0)
     {
-        for (size_t i = 0; i < poolSize; ++i) {
-            void* buf = allocate_numa_local_buffer(topo, bufferBytes, numaNode);
-            buffers_.push_back(buf);
-            freeBuffers_.push(buf);
-        }
+        for (size_t i = 0; i < poolSize_; ++i)
+            buffers_[i] = allocate_numa_local_buffer(topo, bufferBytes, numaNode);
     }
     ~BufferPool() {
         for (void* ptr : buffers_)
             if (ptr) free_numa_local_buffer(topo_, ptr, bufferBytes_);
     }
-    // Get a buffer, blocking if all are in use
+    // Producer: get next buffer in ring (waits if pool is full)
     void* get_buffer() {
-        std::unique_lock<std::mutex> lock(mtx_);
-        while (freeBuffers_.empty()) {
-            cv_.wait(lock);
+        while ((head_ + 1) % poolSize_ == tail_.load(std::memory_order_acquire)) {
+            std::this_thread::yield(); // Wait for consumer
         }
-        void* ptr = freeBuffers_.front();
-        freeBuffers_.pop();
+        void* ptr = buffers_[head_];
+        // Ownership transferred to producer
         return ptr;
     }
-    // Return a buffer to the pool
-    void release_buffer(void* ptr) {
-        std::unique_lock<std::mutex> lock(mtx_);
-        freeBuffers_.push(ptr);
-        lock.unlock();
-        cv_.notify_one();
+    // Producer: after queueing, advance head
+    void commit_buffer() {
+        head_ = (head_ + 1) % poolSize_;
+    }
+    // Consumer: get buffer for consumption
+    void* get_current_consumer_buffer() {
+        // Consumer always reads from buffers_[tail_]
+        return buffers_[tail_];
+    }
+    // Consumer: after processing, advance tail
+    void release_buffer() {
+        tail_ = (tail_ + 1) % poolSize_;
     }
 private:
     std::vector<void*> buffers_;
-    std::queue<void*>  freeBuffers_;
-    std::mutex         mtx_;
-    std::condition_variable cv_;
+    size_t poolSize_;
     size_t bufferBytes_;
     hwloc_topology_t topo_;
+    std::atomic<size_t> head_;
+    std::atomic<size_t> tail_;
 };
-
 
 //=========================
 //        mexFunction
@@ -713,6 +718,8 @@ void mexFunction(int nlhs, mxArray* plhs[],
                     task->compressionTag = compressionTag;
                     task->useTiles       = useTiles;
                     queueForPair.push(std::move(task));
+
+                    bufferPool.commit_buffer(); // <<< ADVANCE after every buffer push!
                 }
                 // Signal end-of-tasks to consumer(s)
                 queueForPair.push(nullptr);
@@ -729,13 +736,13 @@ void mexFunction(int nlhs, mxArray* plhs[],
                     try {
                         write_slice_to_tiff(*task);
                     } catch (const std::exception& ex) {
+                        bufferPool.release_buffer(); // advance consumer tail, regardless of error
                         abortFlag.store(true, std::memory_order_release);
                         std::lock_guard<std::mutex> lock(errorMutex);
                         runtimeErrors.emplace_back(ex.what());
                         break;
                     }
-                    // Return buffer to pool after file write is finished!
-                    bufferPool.release_buffer(task->sliceBuffer);
+                    bufferPool.release_buffer(); // advance consumer tail
                 }
             });
         }
