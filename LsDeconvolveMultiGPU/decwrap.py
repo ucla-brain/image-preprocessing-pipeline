42af077e0e1293a55043252367fe8995a6b95df1
Keivan Moradi <k.moradi@gmail.com>
2025-07-15 10:27:53 -0700

    Unable to communicate with required MathWorks services (error 5201).

--- LsDeconvolveMultiGPU/decwrap.py
+++ LsDeconvolveMultiGPU/decwrap.py
@@ -465,7 +465,7 @@ def main():
         # Windows-compatible MATLAB command
         matlab_cmd = [
             matlab_exec,
-            "-nodesktop -nosplash -nojvm -batch",
+            "-nodesktop -nosplash -nojvm -batch", #
             f"run('{tmp_script_path.stem}')"
         ]
 
