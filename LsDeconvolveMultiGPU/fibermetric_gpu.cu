1958ba7a4313d59aa980106b0f215cfb8f44cb62
Keivan Moradi <k.moradi@gmail.com>
2025-07-29 15:21:10 -0700

    testing fiber metric

--- LsDeconvolveMultiGPU/fibermetric_gpu.cu
+++ LsDeconvolveMultiGPU/fibermetric_gpu.cu
@@ -20,13 +20,13 @@
 #include "gpu/mxGPUArray.h"
 
 #include <cuda_runtime.h>
-#include <cuda.h>
+//#include <cuda.h>
 #include <device_launch_parameters.h>
 
 #include <cfloat>
-#include <cmath>
+//#include <cmath>
 #include <vector>
-#include <cstring>
+//#include <cstring>
 
 #if defined(_WIN32)
     #define STRICMP _stricmp
