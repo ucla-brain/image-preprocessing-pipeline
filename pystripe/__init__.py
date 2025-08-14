f557024b864004666d44bd04ca74ebe49ab85637
Keivan Moradi <k.moradi@gmail.com>
2025-08-13 17:51:32 -0700

    Minor bug fix

--- pystripe/__init__.py
+++ pystripe/__init__.py
@@ -1,3 +1,3 @@
-from pystripe.core import (filter_streaks, filter_streaks_3D, batch_filter, np_gaussian_filter, max_level,
+from pystripe.core import (filter_streaks, filter_streaks_3d, batch_filter, np_gaussian_filter, max_level,
                            foreground_fraction, imread_tif_raw_png, imread_dcimg, imsave_tif, normalize_flat)
 
